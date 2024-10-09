0ed254283cffc24a8b61c063e2a9302fe9c4a208
awkrail <taichitary@gmail.com>
2024-10-09 22:00:35 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firefly',
+    version='0.1',
+    install_requires=['easydict', 'pandas', 'tqdm', 'pyyaml', 'scikit-learn', 'ffmpeg-python',
+                      'pillow', 'sentencepiece', 'transformers', 'timm',
+                      'ftfy', 'regex', 'einops', 'fvcore', 'gradio', 'torchlibrosa', 'librosa',
+                      'clip@git+https://github.com/openai/CLIP.git'],
+    packages=find_packages(exclude=['demo']),
+)
