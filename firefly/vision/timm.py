acc8669bdca6b121efe1a51b1c6b1bf44dfa8d04
awkrail <taichitary@gmail.com>
2024-10-22 16:36:34 +0900

    add timm

--- firefly/vision/timm.py
+++ firefly/vision/timm.py
@@ -2,22 +2,23 @@ import timm
 import torch
 import math
 
-from torchvision.transforms import Compose
+from PIL import Image
+
+from torchvision.transforms import Compose, ToTensor
+from timm.data.transforms_factory import create_transform
+from timm.data import resolve_data_config
 from typing import Optional
 
 from firefly.base_encoder import BaseVisionEncoder
 from firefly.frame_extractor.frame import VideoFrame
 from firefly.model_config import _available_timm_models
 
-import torchvision.models as models
-
 class TimmEncoder(BaseVisionEncoder):
     def __init__(
         self,
         device: str,
         model_path: str,
-        feature_map: bool = False,
-        preprocess_transforms: Optional[Compose] = None):
+        feature_map: bool = False):
         self._model_path: str = model_path
         self._device: str = device
         self._use_feature_map: bool = feature_map
@@ -26,19 +27,22 @@ class TimmEncoder(BaseVisionEncoder):
             raise ValueError(f'{model_path} are not in {self._available_models}.')
     
         self._model = timm.create_model(model_path, pretrained=True).eval().to(self._device)
+        
+        _transforms = create_transform(**resolve_data_config(self._model.default_cfg, model=self._model))
+        _filtered_transforms = [t if not isinstance(t, ToTensor) else lambda x: x / 255.0 for t in _transforms.transforms]
+        self._transforms = Compose(_filtered_transforms)
+
         if not self._use_feature_map:
             self._model = torch.nn.Sequential(*list(self._model.children())[:-1])
-        
-        self._transforms = preprocess_transforms
-        self._model.eval()
 
+        self._model.eval()
 
     @torch.no_grad()
     def encode_video(
         self,
         video_frames: VideoFrame,
         batch_size: int = 60) -> torch.Tensor:
-        preprocessed_frames = self._transforms(video_frames.frames) if self._transforms is not None else video_frames.frames
+        preprocessed_frames = self._transforms(video_frames.frames)
         n_frames = len(video_frames)
         n_batch = int(math.ceil(n_frames / batch_size))
         video_features = []
